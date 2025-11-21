2c674798da7b3f699419dd713520497b7b48cb6c
Kanishka <02fe23bcs164@kletech.ac.in>
2025-11-21 13:48:23 +0530

    Fix footer duplication, update dev script to use npx nodemon, and integrate frontend with backend API

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,21 +4,15 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="banner">
-          <div className="left">Kanishka</div>
-          <div className="right">
-            <p>Sadashiv Nagar ,belgaum</p>
-            <p>Open: 05:00 PM - 12:00 AM</p>
-          </div>
+      <div className="container footer-inner">
+        <div className="left">
+          <h3>Kanishka</h3>
+          <p>Sadashiv Nagar, Belgaum</p>
+          <p>Open: 05:00 PM - 12:00 AM</p>
         </div>
-        <div className="banner">
-          <div className="left">
-            <p>Developed By KANISHKA</p>
-          </div>
-          <div className="right">
-            <p>All Rights Reserved By KANISHKANAIK.</p>
-          </div>
+        <div className="right">
+          <p>Developed By KANISHKA</p>
+          <p>All Rights Reserved By KANISHKANAIK.</p>
         </div>
       </div>
     </footer>
